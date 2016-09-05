3e70d660fb966683010164e2004eac73f2cb3f2e
valoriedodge <valoriejones@gmail.com>
2016-09-05 09:36:35 -0700

    assignment 11 complete

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -5,19 +5,7 @@
              if (Number.isNaN(seconds)) {
                  return '-:--';
              }
-             var wholeSeconds = Math.floor(seconds);
-             var minutes = Math.floor(wholeSeconds / 60);
-             var remainingSeconds = wholeSeconds % 60;
- 
-             var output = minutes + ':';
- 
-             if (remainingSeconds < 10) {
-                 output += '0';   
-             }
- 
-             output += remainingSeconds;
-
-             return output;
+             return buzz.toTimer(seconds);
         };
     }
     
